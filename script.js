5bf40d0ea633484936b2a262416b7f4b0daf6ba2
Fezbot3000 <matthewjakekane@icloud.com>
2024-08-05 18:02:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -123,8 +123,8 @@ document.getElementById('billsForm').addEventListener('submit', function(event) 
     if (billIndex === '') {
         bills.push({ name: billName, amount: billAmount, frequency: billFrequency, date: billDate });
     } else {
-        bills[billIndex] = { name: billName, amount: billAmount, frequency: billFrequency, date: billDate });
-    }
+        bills[billIndex] = { name: billName, amount: billAmount, frequency: billFrequency, date: billDate
+    };
 
     saveToLocalStorage();
     updateBillsTable();
