1bd8b9cb448dec47bfc9e0da7ca449f3731e30e6
Fezbot3000 <matthewjakekane@icloud.com>
2024-08-24 17:57:35 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Initialize variables and load data from localStorage
+// Initialize variables and load data from localStorage change
 let bills = JSON.parse(localStorage.getItem('bills')) || [];
 let payFrequency = localStorage.getItem('payFrequency') || '';
 let income = parseFloat(localStorage.getItem('income')) || 0;
